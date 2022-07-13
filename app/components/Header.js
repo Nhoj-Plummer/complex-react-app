593b3b5066af5349e88638224ca48e5ef859819b
Nhoj <nhojplummer@yahoo.com>
2022-07-13 18:01:56 -0400

    Rendering Different Components Depending on State

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,8 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import HeaderLoggedOut from "./HeaderLoggedOut"
+import HeaderLoggedIn from "./HeaderLoggedin"
 
 function Header() {
+  const [loggedIn, setLoggedIn] = useState()
+
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -12,7 +15,7 @@ function Header() {
             ComplexApp{" "}
           </Link>
         </h4>
-        <HeaderLoggedOut />
+        {loggedIn ? <HeaderLoggedIn setLoggedIn={setLoggedIn} /> : <HeaderLoggedOut setLoggedIn={setLoggedIn} />}
       </div>
     </header>
   )
