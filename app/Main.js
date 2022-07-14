c78c8eea8b7760b38c12eb1f37614d1a43612ce5
Nhoj <nhojplummer@yahoo.com>
2022-07-13 20:07:06 -0400

    Made it view single post screen

--- app/Main.js
+++ app/Main.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
 
-//My COmponents
+// My Components
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
 import Home from "./components/Home"
@@ -10,8 +12,7 @@ import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
 import CreatePost from "./components/CreatePost"
-import Axios from "axios"
-Axios.defaults.baseURL = "http://localhost:8080"
+import ViewSinglePost from "./components/ViewSinglePost"
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")))
@@ -21,9 +22,10 @@ function Main() {
       <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
         <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+        <Route path="/post/:id" element={<ViewSinglePost />} />
         <Route path="/create-post" element={<CreatePost />} />
-        <Route path="about-us" element={<About />} />
-        <Route path="terms" element={<Terms />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
       </Routes>
       <Footer />
     </BrowserRouter>
