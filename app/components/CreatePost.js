d2db932ecb7c2a76870639506b6482f79c7342fa
Nhoj <nhojplummer@yahoo.com>
2022-07-13 20:25:03 -0400

    Added Flash Message to 'Create Post' Section (Will update later)

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom"
 import Page from "./Page"
 import Axios from "axios"
 
-function CreatePost() {
+function CreatePost(props) {
   const [title, setTitle] = useState()
   const [body, setBody] = useState()
   const navigate = useNavigate()
@@ -13,6 +13,7 @@ function CreatePost() {
     try {
       const response = await Axios.post("/create-post", { title, body, token: localStorage.getItem("complexappToken") })
       // Redirect to new post url
+      props.addFlashMessage("Congrats, you successfully created a post")
       navigate(`/post/${response.data}`)
       console.log("New post was created")
     } catch (e) {
